cd2c7d3b343ebfea12d4dbc0bfcb92569634175f
ZEDD2468 <victoradebayo360@gmail.com>
2023-08-24 18:10:36 +0100

    Unittests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,33 +1,25 @@
 /* eslint-disable jest/valid-expect */
 /* eslint-disable jest/expect-expect */
 /* eslint-disable jest/prefer-expect-assertions */
-const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const chai = require('chai');
+const calculateNumber = require('./2-calcul_chai').calculateNumber;
 
+const expect = chai.expect;
 // You can assume a and b are always number
 
-describe('calculateNumber', () => {
-  it('handles adding two rounded numbers when type is SUM', () => {
-    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-    expect(calculateNumber('SUM', -1.2, -3.7)).to.equal(-5);
+describe("calculateNumber", function() {
+  it("should calculate the sum of 2 numbers", function() {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    expect(calculateNumber('SUM', 10.6, 4.1)).to.equal(15);
   });
 
-  it('handles subtracting two rounded numbers when type is SUBTRACT', () => {
-    expect(calculateNumber('SUBTRACT', 5.9, 2.3)).to.equal(4);
-    expect(calculateNumber('SUBTRACT', -5.9, -2.3)).to.equal(-4);
+  it("should calculate the substraction of 2 numbers", function () {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    expect(calculateNumber('SUBTRACT', -1, -1)).to.equal(0);
   });
 
-  it('handles dividing two rounded numbers when type is DIVIDE', () => {
-    expect(calculateNumber('DIVIDE', 8, 2)).to.equal(4);
-    // expect(calculateNumber('DIVIDE', 5.0, 3.0)).toEqual(2);
-  });
-
-  it('returns Error when dividing by 0', () => {
-    expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
-    expect(calculateNumber('DIVIDE', -8, 0)).to.equal('Error');
-  });
-
-  it('throws an error when an invalid type is provided', () => {
-    expect(() => calculateNumber('INVALID_TYPE', 1, 2)).to.throw(Error);
+  it("should divide the divide a number by another", function () {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
 });
