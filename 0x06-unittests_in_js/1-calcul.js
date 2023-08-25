6e0d041a9a644a2715c78c48378d3f5801be695b
ZEDD2468 <victoradebayo360@gmail.com>
2023-08-25 02:08:49 +0100

    Backend

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,13 +1,32 @@
-module.exports = {
-  calculateNumber: function (type, a, b) {
-    aRound = Math.round(a);
-    bRound = Math.round(b);
+/**
+ * Performs a mathematical operation on two rounded numbers
+ * @param {string} type - The type of operation to perform (SUM, SUBTRACT,
+ * or DIVIDE)
+ * @param {number} a - The first number to be rounded and used in the
+ * operation
+ * @param {number} b - The second number to be rounded and used in the
+ * operation
+ * @returns {number|string} The result of the operation, or "Error" if
+ * the operation is division and b is rounded to 0
+ */
+function calculateNumber(type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
 
-    if (type === 'SUM') return aRound + bRound;
-    else if (type === 'SUBTRACT') return aRound - bRound;
-    else if (type === 'DIVIDE') {
-      if (bRound === 0) return 'Error';
-      return aRound / bRound;
-    }
+  switch (type) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA - roundedB;
+    case 'DIVIDE':
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+
+    default:
+      throw new Error(`Invalid type: ${type}`);
   }
 }
+
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,31 +1,32 @@
-const assert = require("assert");
-const { it, describe } = require("mocha");
-const calculateNumber = require("./1-calcul").calculateNumber;
+/* eslint-disable jest/expect-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
 
-describe("#calculateNumber", function() {
+// You can assume a and b are always number
 
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("SUM", 1, 2);
-      assert.strictEqual(res, 3);
-    });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("SUBTRACT", 1.4, 2.2);
-      assert.strictEqual(res, -1);
-    });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("SUBTRACT", 4.9, 2.7);
-      assert.strictEqual(res, 2);
-    });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 4, 2);
-      assert.strictEqual(res, 2);
-    });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 1.7, 0);
-      assert.strictEqual(res, "Error");
-    });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 1.4, 4.6);
-      assert.strictEqual(res, 0.2);
-    });
+describe('calculateNumber', () => {
+  it('handles adding two rounded numbers when type is SUM', () => {
+    assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+    assert.equal(calculateNumber('SUM', -1.2, -3.7), -5);
+  });
+
+  it('handles subtracting two rounded numbers when type is SUBTRACT', () => {
+    assert.equal(calculateNumber('SUBTRACT', 5.9, 2.3), 4);
+    assert.equal(calculateNumber('SUBTRACT', -5.9, -2.3), -4);
+  });
+
+  it('handles dividing two rounded numbers when type is DIVIDE', () => {
+    assert.equal(calculateNumber('DIVIDE', 8, 2), 4);
+    // assert.equal(calculateNumber('DIVIDE', 5.0, 3.0), 2);
+  });
+
+  it('returns Error when dividing by 0', () => {
+    assert.equal(calculateNumber('DIVIDE', 4, 0), 'Error');
+    assert.equal(calculateNumber('DIVIDE', -8, 0), 'Error');
+  });
+
+  it('throws an error when an invalid type is provided', () => {
+    assert.throws(() => calculateNumber('INVALID_TYPE', 1, 2), Error);
+  });
 });
